14f252e11a6a15f63409cf643c369a1983ac8aea
Savio Dimatteo <darksmo@gmail.com>
2014-06-19 21:49:22 +0200

    Use global window object to refer to the document

--- assets/js/inject_livereload.js
+++ assets/js/inject_livereload.js
@@ -1,6 +1,6 @@
 (function () {
-    var script = document.createElement('script');
+    var script = window.document.createElement('script');
     script.src = 'http://localhost:35729/livereload.js';
-    var body = document.getElementsByTagName('body')[0];
+    var body = window.document.getElementsByTagName('body')[0];
     body.appendChild(script);
 })();
